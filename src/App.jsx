80037e38c26165ec20cc959830a22275ec4cb20f
Kundan Tripathi <kundantripathi1512@gmail.com>
2025-07-18 13:21:03 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
-import Card from "./Components/Card";
-import Header from "./Components/Header";
-import Main from "./Components/Main";
+import Card from "./FaqComponents/Card";
+import Header from "./FaqComponents/Header";
+import Main from "./FaqComponents/Main";
 import questions from "./questionsList";
 
 const App = () => {
-  const [openId, setOpenId] = useState(questions[0].questionID);
+  //------------------for faq component-----------------------------------------------
+  // const [openId, setOpenId] = useState(questions[0].questionID);
 
-  function handleClick(id){
-    setOpenId(prev => id === prev ? prev : id)
-  };
+  // function handleClick(id){
+  //   setOpenId(prev => id === prev ? prev : id)
+  // };
+  //------------------------------------------------------------------------------
+
+
+  //-----------------------for mortgage component-----------------------------------
+  //---------------------------------------------------------------------------------
 
   return (
     <div className="flex justify-center items-center w-full h-screen">
-      <Main>
+      {/*----------------------------------------------------------------*/
+      /* <Main>
         <Header />
         {questions.map((item) => (
           <Card
@@ -23,7 +30,27 @@ const App = () => {
             handleClick = {handleClick}
           />
         ))}
-      </Main>
+      </Main> */
+      /*----------------------------------------------------------------*/
+      }
+
+
+      <main className="flex flex-col gap-5 items-start w-[50%] h-[70%] border p-[2vw]">
+        <div className="">
+          <h1 className="text-[2vw] font-bold">Principle Loan Amount:-</h1>
+          <input type="text" className="bg-amber-100 border p-2 rounded-xl"/>
+        </div>
+
+        <div className="">
+          <h1 className="text-[2vw] font-bold">Interest rate:-</h1>
+          <input type="text" className="bg-amber-100 border p-2 rounded-xl"/>
+        </div>
+
+        <div className="">
+          <h1 className="text-[2vw] font-bold">Principle Loan Amount:-</h1>
+          <input type="text" className="bg-amber-100 border p-2 rounded-xl"/>
+        </div>
+      </main>
     </div>
   );
 };
